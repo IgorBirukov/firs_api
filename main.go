8ab753805de64706ee5e8bd1f5e8f9e58bbfb522
Ibirukov <igor_land@mail.ru>
2025-02-13 15:22:29 +0600

    исправление замечание по 2 задаче

--- main.go
+++ main.go
@@ -16,30 +16,29 @@ type task struct {
 var mTask string
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	//var tasks []Task
+	var tasks []Task
 	w.Header().Set("Content-Type", "application/json")
 	var result []struct {
 		Task string
 	}
-	DB.Table("tasks").Select("task").Scan(&result)
-	//DB.Model(&Task{}).Select("task").Find(&tasks)
-	//DB.Find(&tasks)
-	json.NewEncoder(w).Encode(result)
+
+	DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
 	fmt.Println(result)
-	//fmt.Fprintf(w, "hello, %s", mTask)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		n0 := task{Task: ""}
 
+		n0 := task{Task: ""}
+		w.Header().Set("Content-Type", "application/json")
 		err := json.NewDecoder(r.Body).Decode(&n0)
 		if err != nil {
 			panic(err)
 		}
 		mTask = n0.Task
 		DB.Create(&n0)
-
+		json.NewEncoder(w).Encode(&n0)
 		fmt.Println("Получено значени:", n0.Task)
 
 	}
